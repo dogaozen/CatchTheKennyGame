083ab15d439586e53a781c466ee2cb4a14543bb7
Doğa Özen <dgaozen@gmail.com>
2025-03-09 05:25:32 +0300

    Initial Commit

---
+++ CatchTheKennyGame/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  CatchTheKennyGame
+//
+//  Created by pc on 9.03.2025.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func startButtonClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toViewController", sender: nil)
+        
+    }
+    
+}
